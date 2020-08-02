67cd2b33953e8a805ab9684dae0215004b100807
Alvarez Mata <luis.c.alvarez.mata@intel.com>
2020-08-01 23:20:48 -0600

    Update rgb2yuv_intrinsics.c

--- project_2/Prototyping/rgb2yuv_intrinsics.c
+++ project_2/Prototyping/rgb2yuv_intrinsics.c
@@ -9,8 +9,6 @@
  *
  */
 
-//gcc rgb2yuv_c.c -o rgb2yuv_c `pkg-config --cflags opencv4 --libs opencv4`
-//./rgb2yuv_c -i image.rgb -o outputC.yuv
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,106 +19,134 @@
 #define IMAGE_WIDTH  640
 #define IMAGE_HEIGHT 480
 
-int16x4_t  rgb2yuvPixel (int32_t  R, int32_t  G, int32_t  B){
-	
-	int32_t  Y, V, U;
-	int32_t aux1, aux2, aux3, aux4;
-
-	uint32_t  pixel32;
-	unsigned char *pixel = (unsigned char *)&pixel32;
-
-	aux1 = vmul_u32(0.257, R);
-	aux2 = vmul_u32(0.504, G);
-	aux3 = vmul_u32(0.098, B);
-	aux4 = vadd_u32(aux1, aux2);
-	aux4 = vadd_u32(aux4, aux3);
-	Y = vadd_u32(aux4, 16);
-	//Y = (0.257 * R) + (0.504 * G) + (0.098 * B) + 16;
-	
-	aux1 = vmul_u32(0.439, R);
-	aux2 = vmul_u32(0.368, G);
-	aux2 = vmul_u32(aux2);
-	aux3 = vmul_u32(0.071, B);
-	aux3 = vmul_u32(aux3);
-	aux4 = vsub_u32(aux1, aux2);
-	aux4 = vsub_u32(aux4, aux3);
-	V = vadd_u32(aux4, 128);	
-	//V =  (0.439 * R) - (0.368 * G) - (0.071 * B) + 128;
-	
-	aux1 = vmul_u32(0.148, R);
-	aux1 = vneg_u32(aux1);
-	aux2 = vmul_u32(0.368, G);
-	aux2 = vmul_u32(aux2);
-	aux3 = vmul_u32(0.071, B);
-	aux3 = vmul_u32(aux3);
-	aux4 = vsub_u32(aux1, aux2);
-	aux4 = vsub_u32(aux4, aux3);
-	U = vadd_u32(aux4, 128);
-	//U = -(0.148 * R) - (0.291 * G) + (0.439 * B) + 128;
-
- 	if (Y > 255) {
-		Y = 255;
-	}
-
-	if (U > 255) {
-		U = 255;
-	}
-
-	if (V > 255) {
-		V = 255;
-	}
-
-	if (Y < 0) {
-		Y = 0;
-	}
-	
-	if (U < 0) {
-		U = 0;
-	}
-
-	if (V < 0) {
-		V = 0;
-	}
-
-	pixel[0] = Y;
-   	pixel[1] = U;
-  	pixel[2] = V;
-   	pixel[3] = 0;
-
-	return pixel32;
-}
-
 void rgb2yuv(char *input_image, char *output_image){
 
 	FILE *in, *out;
-	int32_t R, G, B, i, size;	
-	uint32_t pixelRGB, pixel32;
-	unsigned char pixelYUV[3];
+	int totalBytes = IMAGE_WIDTH * IMAGE_HEIGHT * 3;
+    int totalPixels = IMAGE_WIDTH * IMAGE_HEIGHT;
+	
+	unsigned char buffer_in[totalBytes];
+    signed char buffer_out[totalBytes];
+	
+	//To save results 
+	uint8x16x3_t rgb_x3x16_tmp;
+	uint8x16x3_t yuv_x3x16_tmp;
+	
+	uint8_t *rgb_ptr = buffer_in;
+    uint8_t *yuv_ptr = buffer_out;
 
+	//Open files for RGB and YUV
 	in = fopen(input_image, "rb");
 	out = fopen(output_image, "wb");
+	
 	if (!in  ||  !out) {
 		printf("Error..\n");
 	}
 	
-	size = IMAGE_WIDTH*IMAGE_HEIGHT;
-	for(i=0; i<size; i++){
-		fread(&pixelRGB, 3, 1, in);
-		R  = ((pixelRGB & 0x000000ff));
-		G  = ((pixelRGB & 0x0000ff00)>>8);
-		B  = ((pixelRGB & 0x00ff0000)>>16);
+	//Loading the buffer
+	fread(buffer_in,totalBytes,1,in);
 	
-		pixel32 = rgb2yuvPixel(R, G, B);
-		pixelYUV[0] = (pixel32 & 0x000000ff);
-		pixelYUV[1] = (pixel32 & 0x0000ff00) >> 8;
-		pixelYUV[2] = (pixel32 & 0x00ff0000) >> 16;
+	//go over the image every 16 pixels
+	for(int i = 0; i<totalPixels/16; i++)
+		
+		//loading 16 pixels (R, G, B components)
+		rgb_x3x16_tmp = vld3q_u8(rgb_ptr+i*16*3);
+		
+		//Dividing the data of the vector for each color component
+		uint8x8_t high_r = vget_high_u8(rgb_x3x16_tmp.val[2]);
+		uint8x8_t low_r = vget_low_u8(rgb_x3x16_tmp.val[2]);
+		uint8x8_t high_g = vget_high_u8(rgb_x3x16_tmp.val[1]);
+		uint8x8_t low_g = vget_low_u8(rgb_x3x16_tmp.val[1]);
+		uint8x8_t high_b = vget_high_u8(rgb_x3x16_tmp.val[0]);
+		uint8x8_t low_b = vget_low_u8(rgb_x3x16_tmp.val[0]);
+		
+		//Vector reinterpret cast operation
+		int16x8_t signed_high_r = vreinterpretq_s16_u16(vaddl_u8(high_r, vdup_n_u8(0)));
+		int16x8_t signed_low_r = vreinterpretq_s16_u16(vaddl_u8(low_r, vdup_n_u8(0)));
+		int16x8_t signed_high_g = vreinterpretq_s16_u16(vaddl_u8(high_g, vdup_n_u8(0)));
+		int16x8_t signed_low_g = vreinterpretq_s16_u16(vaddl_u8(low_g, vdup_n_u8(0)));
+		int16x8_t signed_high_b = vreinterpretq_s16_u16(vaddl_u8(high_b, vdup_n_u8(0)));
+		int16x8_t signed_low_b = vreinterpretq_s16_u16(vaddl_u8(low_b, vdup_n_u8(0)));
+		
+		//Y component
+		//Duplicate vector element to scalar
+		uint8x8_t coef1 = vdup_n_u8(66);
+        uint8x8_t coef2 = vdup_n_u8(129);
+        uint8x8_t coef3 = vdup_n_u8(25);
+		
+		
+		uint16x8_t high_y = vmull_u8(high_r, coef1); //Unsigned Multiply long 
+		high_y = vmlal_u8(high_y, high_g, coef2); //Unsigned Multiply-Add Long
+		high_y = vmlal_u8(high_y, high_b, coef3); //Unsigned Multiply-Add Long
+		uint16x8_t low_y  = vmull_u8(low_r, coef1); //Unsigned Multiply long 
+		low_y = vmlal_u8(low_y, low_g, coef2); //Unsigned Multiply-Add Long
+		low_y = vmlal_u8(low_y, low_b, coef3); //Unsigned Multiply-Add Long
+		
+		high_y = vaddq_u16(high_y, vdupq_n_u16(128)); //Add
+		low_y = vaddq_u16(low_y, vdupq_n_u16(128)); //Add
 
-		fwrite(pixelYUV, 3, 1, out);
-	}
+		//Join two smaller vectors into a single larger vector
+		//Unsigned saturating shift right narrow
+		uint8x16_t y = vcombine_u8(vqshrn_n_u16(low_y, 8), vqshrn_n_u16(high_y, 8)); 
+		yuv_x3x16_tmp.val[0] = y; //-> Component Y
+		
+		
+		//V component
+		//Duplicate vector element to scalar
+		scoef1 = vdupq_n_s16(-38);
+		scoef2 = vdupq_n_s16(-74);
+		scoef3 = vdupq_n_s16(112);
+		
+		int16x8_t high_v = vmulq_s16(signed_high_r, scoef1); //Multiply
+		high_v = vmlaq_s16(high_v, signed_high_g, scoef2); //Multiply-add to accumulator
+		high_v = vmlaq_s16(high_v, signed_high_b, scoef3); //Multiply-add to accumulator
+		int16x8_t low_v = vmulq_s16(signed_low_r, scoef1); //Multiply
+		low_v = vmlaq_s16(low_v, signed_low_g, scoef2); //Multiply-add to accumulator
+		low_v = vmlaq_s16(low_v, signed_low_b, scoef3); //Multiply-add to accumulator
+		
+		high_v = vaddq_s16(high_v, vdupq_n_s16(128)); //Add
+		low_v = vaddq_s16(low_v, vdupq_n_s16(128)); //Add
+
+		//Join two smaller vectors into a single larger vector
+		//Unsigned saturating shift right narrow
+		int8x16_t v = vcombine_s8(vqshrn_n_s16(low_v, 8), vqshrn_n_s16(high_v, 8));
+		v = vaddq_s8(v, vdupq_n_s8(128)); //Add
+		yuv_x3x16_tmp.val[2] = vreinterpretq_u8_s8(v); //-> Component V
+		
+		
+		//U component
+		//Duplicate vector element to scalar
+		int16x8_t scoef1 = vdupq_n_s16(112);
+		int16x8_t scoef2 = vdupq_n_s16(-94);
+		int16x8_t scoef3 = vdupq_n_s16(-18);
+		
+		int16x8_t high_u = vmulq_s16(signed_high_r, scoef1); //Multiply
+		high_u = vmlaq_s16(high_u, signed_high_g, scoef2); //Multiply-add to accumulator
+		high_u = vmlaq_s16(high_u, signed_high_b, scoef3); //Multiply-add to accumulator
+		int16x8_t low_u = vmulq_s16(signed_low_r, scoef1); //Multiply
+		low_u = vmlaq_s16(low_u, signed_low_g, scoef2); //Multiply-add to accumulator
+		low_u = vmlaq_s16(low_u, signed_low_b, scoef3); //Multiply-add to accumulator
+
+		high_u = vaddq_s16(high_u, vdupq_n_s16(128)); //Add
+		low_u = vaddq_s16(low_u, vdupq_n_s16(128)); //Add
+
+		//Join two smaller vectors into a single larger vector
+		//Unsigned saturating shift right narrow
+		int8x16_t u = vcombine_s8(vqshrn_n_s16(low_u, 8), vqshrn_n_s16(high_u, 8));
+		u = vaddq_s8(u, vdupq_n_s8(128)); //Add
+		yuv_x3x16_tmp.val[1] = vreinterpretq_u8_s8(u); //-> Component U
 	
+
+		vst3q_u8(yuv_ptr, yuv_x3x16_tmp); //Store multiple 3-element structures from three registers
+		yuv_ptr += 3*16; //Move to the next block of registers
+			
+	}
+	//write the file YUV
+    fwrite(buffer_out,totalBytes,1,write_ptr);  
+	
+	//Close files
 	fclose(in);
 	fclose(out);
-
 }
 
 
